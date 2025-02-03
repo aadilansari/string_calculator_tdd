b9ac6045b3e1792eae6f5928875316b70b881216
aadilansari <aadil.ansari8@gmail.com>
2025-02-03 21:49:59 +0530

    add[test and regex]: to handle new line between numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,7 +3,7 @@ class StringCalculator {
     if (numbers.isEmpty) {
       return 0;
     }
-    var parts = numbers.split(',');
+    var parts = numbers.split(RegExp(r'[,\n]'));
     var sum = 0;
     for (var part in parts) {
       sum += int.parse(part);
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -16,4 +16,13 @@ void main() {
     var calculator = StringCalculator();
     expect(calculator.add('1,5'), 6);
   });
+  test('Two number returns sum', () {
+    var calculator = StringCalculator();
+    expect(calculator.add('1,5'), 6);
+  });
+
+  test('New lines between numbers returns sum', () {
+    var calculator = StringCalculator();
+    expect(calculator.add('1\n2,3'), 6);
+  });
 }
