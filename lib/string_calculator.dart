e8b1548ec414bf583fd0aadb2a3a23ff908a3ecb
aadilansari <aadil.ansari8@gmail.com>
2025-02-03 21:55:04 +0530

    add[method]: to handle custom delimeter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,16 @@ class StringCalculator {
     if (numbers.isEmpty) {
       return 0;
     }
+    if (numbers.startsWith('//')) {
+      var delimeter = numbers[2];
+      numbers = numbers.substring(4);
+      var parts = numbers.split(delimeter);
+      var sum = 0;
+      for (var part in parts) {
+        sum += int.parse(part);
+      }
+      return sum;
+    }
     var parts = numbers.split(RegExp(r'[,\n]'));
     var sum = 0;
     for (var part in parts) {
